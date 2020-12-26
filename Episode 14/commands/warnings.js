664f808ca0c2df71353f47640ab791376cd220e1
BelyGaming <76529016+BelyGaming@users.noreply.github.com>
2020-12-26 12:31:29 +0200

    Update warnings.js

--- Episode 14/commands/warnings.js
+++ Episode 14/commands/warnings.js
@@ -3,8 +3,7 @@ const db = require('quick.db');
 module.exports = {
     name: "warnings",
     description: "Check a users warnings",
-
-    async run (client, message, args){
+execute(message, args){
         const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
 
 
